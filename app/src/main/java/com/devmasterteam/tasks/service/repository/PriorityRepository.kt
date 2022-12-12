8f53cea87f1661da141cee99a70263b59374baa5
Caseca93 <icaseca93@gmail.com>
2022-12-12 08:20:03 -0300

    TASKS - Criando repositorio base

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -13,7 +13,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(val context: Context) {
+class PriorityRepository(val context: Context) : BaseRepository() {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val database = TaskDatabase.getDatabase(context).priorityDAO()
@@ -22,11 +22,7 @@ class PriorityRepository(val context: Context) {
         val call = remote.list()
         call.enqueue(object : Callback<List<PriorityModel>>{
             override fun onResponse(call: Call<List<PriorityModel>>, r: Response<List<PriorityModel>>) {
-                if(r.code() == TaskConstants.HTTP.SUCCESS){
-                    r.body()?.let { listener.onSucess(it) }
-                } else {
-                    listener.onFailure(failResponse(r.errorBody()!!.string()))
-                }
+                handleResponse(r, listener)
             }
 
             override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
@@ -43,9 +39,5 @@ class PriorityRepository(val context: Context) {
     fun save(list: List<PriorityModel>){
         database.clear()
         database.save(list)
-    }
-
-    private fun failResponse(str: String): String{
-        return Gson().fromJson(str, String::class.java)
     }
 }
